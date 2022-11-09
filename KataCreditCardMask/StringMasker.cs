156235cb57dc0fe4917af1dfcadfa7ac79811500
Jim Pelletier <mail@thejimmyp.com>
2022-11-10 08:52:32 +1100

    refactoring

--- KataCreditCardMask/StringMasker.cs
+++ KataCreditCardMask/StringMasker.cs
@@ -3,6 +3,7 @@ namespace KataCreditCardMask;
 public class StringMasker
 {
     private const int UnmaskedCharLength = 4;
+    private const char MaskingChar = '#';
 
     public static string Mask(string input)
     {
@@ -11,12 +12,21 @@ public class StringMasker
 
     private static string GetMaskedSection(string input)
     {
-        var maskedLength = input.Length - UnmaskedCharLength;
+        var maskedLength = GetMaskedLength(input);
+        
         if (maskedLength < 0)
-            maskedLength = 0;
+        {
+            return string.Empty;
+        }
 
-        var maskedSection = new string('#', maskedLength);
+        var maskedSection = new string(MaskingChar, maskedLength);
+        
         return maskedSection;
+    }
+
+    private static int GetMaskedLength(string input)
+    {
+        return input.Length - UnmaskedCharLength;
     }
 
     private static string GetUnmaskedSection(string input)
