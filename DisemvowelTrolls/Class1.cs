acb010ecd412cb5a725a4b1e463b2c1257898f0b
Arthur Apanovics <26443901+arthur-apanovics@users.noreply.github.com>
2022-11-17 10:13:11 +1300

    Add missing files

---
+++ DisemvowelTrolls/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace DisemvowelTrolls;
+
+public class Class1
+{
+}
---
+++ DisemvowelTrolls/DisemvowelTrollsUnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace DisemvowelTrollsUnitTests;
+
+public class UnitTest1
+{
+    // "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+    [Fact]
+    public void Test1()
+    {
+        var input = "This website is for losers LOL!";
+        var expected = "Ths wbst s fr lsrs LL!";
+
+        var actual = VowelOperations.Remove(input);
+
+        actual.Should().Be(expected);
+    }
+}
